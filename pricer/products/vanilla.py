8d970f80ea886fa80536ba001241e83a7ed08d86
Endoresuu <164026349+Endoresuu@users.noreply.github.com>
2025-12-03 11:21:38 +0000

    ouiiiii

--- pricer/products/vanilla.py
+++ pricer/products/vanilla.py
@@ -1,9 +1,17 @@
 from .base import Option
 
 class EuropeanCall(Option):
+    def __init__(self, K: float, T: float):
+        super().__init__(strike=K, maturity=T)
+
     def payoff(self, ST: float) -> float:
         return max(ST - self.K, 0.0)
 
+
 class EuropeanPut(Option):
+    def __init__(self, K: float, T: float):
+        super().__init__(strike=K, maturity=T)
+
     def payoff(self, ST: float) -> float:
         return max(self.K - ST, 0.0)
+
